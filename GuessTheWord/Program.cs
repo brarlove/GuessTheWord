596edee05f93e84d820ff1741fcf519e97aab855
Love <lovemani0511@gmail.com>
2018-10-04 12:36:25 -0400

    redid the game logic to make it work

--- GuessTheWord/Program.cs
+++ GuessTheWord/Program.cs
@@ -11,66 +11,40 @@ namespace GuessTheWord
         static void Main(string[] args)
         {
             string[] words = new string[]
-            {
+{
                 "trident",
                 "apple",
                 "google",
                 "crest",
                 "car",
                 "phone"
-            };
+};
 
             Random random = new Random();
             int randomNum = random.Next(0, 5);
             int chances = 4;
+            bool win = false;
             string guessWord = words[randomNum];
             int numOfCharsInWord = guessWord.Count();
-            Console.WriteLine("Welcome to GuessTheWord. You have 4 chances to guess the letters of the word. Good Luck!");
-            string underScore = "";
-            string word = "";
-            for(int i = 0; i < numOfCharsInWord; i++)
+            Console.WriteLine("Welcome to GuessTheWord. You have 4 chances to guess the whole word. Good Luck!");
+            while (chances > 0 && win == false)
             {
-                underScore += "_ ";
-            }
-            while(chances > 0)
-            {
-                Console.WriteLine(underScore);
-                Console.WriteLine("Chances left: " + chances);
-                Console.Write("Please guess the letter: ");
-                char letterGuess = Convert.ToChar(Console.ReadLine());
-                bool match = false;
-                var positions = new List<int>();
-                positions.Clear();
-                for (int i = 0; i < guessWord.Length; i++)
+                Console.WriteLine("This word has " + numOfCharsInWord + " characters and you have " + chances + " chances left");
+                string answer = Console.ReadLine();
+                if (answer == guessWord)
                 {
-                    Console.WriteLine(guessWord[i]);
-                    if (Convert.ToChar(guessWord[i]) == letterGuess)
-                    {
-                        match = true;
-                        positions.Add(i);
-                    }
-                    else
-                    {
-                        match = false;
-                    }
-                }
-                Console.WriteLine(false);
-                if(match == false)
-                {
-                    chances--;
+                    Console.WriteLine("Congratulations! You won!");
+                    win = true;
                 }
                 else
                 {
-                    foreach(var position in positions)
-                    {
-                        underScore.Remove(position, 1);
-                        underScore.Insert(position, letterGuess.ToString());
-                        Console.WriteLine("This letter is correct and goes at position: " + position);
-                    }
+                    chances--;
+
                 }
+
                 if (chances == 0)
                 {
-                    Console.WriteLine("Sorry you lost. The correct word is: " + guessWord);
+                    Console.WriteLine("Sorry you lost. The correct word was " + guessWord);
                 }
             }
         }
